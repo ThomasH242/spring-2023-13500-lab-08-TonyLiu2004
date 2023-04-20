154fec3b26a7daee8e0412edd14232203417f612
TonyLiu2004 <104866433+TonyLiu2004@users.noreply.github.com>
2023-04-20 12:52:16 -0400

    Updated spacing in main.cpp

--- main.cpp
+++ main.cpp
@@ -32,11 +32,11 @@ void invertHalf(string input){
 
 	for(int row = 0; row < h; row++) {
 		for(int col = 0; col < w; col++) {
-            if(col > w/2){
-			    out[row][col] = abs(255- img[row][col]);
-            }else{
-                out[row][col] = img[row][col];
-            }
+		    if(col > w/2){
+				    out[row][col] = abs(255- img[row][col]);
+		    }else{
+			out[row][col] = img[row][col];
+		    }
 		}
 	}
 
@@ -52,11 +52,11 @@ void box(string input){
 
 	for(int row = 0; row < h; row++) {
 		for(int col = 0; col < w; col++) {
-            if((row > h/4) && (row<h*0.75)&& (col < w*0.75) && (col > w/4)){
-                out[row][col] = 255;
-            }else{
-                out[row][col] = img[row][col];
-            }
+		    if((row > h/4) && (row<h*0.75)&& (col < w*0.75) && (col > w/4)){
+			out[row][col] = 255;
+		    }else{
+			out[row][col] = img[row][col];
+		    }
 		}
 	}
 
@@ -72,14 +72,14 @@ void frame(string input){
 
 	for(int row = 0; row < h; row++) {
 		for(int col = 0; col < w; col++) {
-            if( ((col== int(w*0.25))) && ((row < int(h*0.75)) && (row>int(h*0.25))) || //left
-                ((col== int(w*0.75))) && ((row < int(h*0.75)) && (row>int(h*0.25))) ||// right
-                ((row == int(h/4)) && ((col > w/4) && (col < int(w*0.75)))) || // top
-                ((row == int(h*0.75)) && ((col > w/4) && (col < int(w*0.75)))) ){ //bottom
-                out[row][col] = 255;
-            }else{
-                out[row][col] = img[row][col];
-            }
+		    if( ((col== int(w*0.25))) && ((row < int(h*0.75)) && (row>int(h*0.25))) || //left
+			((col== int(w*0.75))) && ((row < int(h*0.75)) && (row>int(h*0.25))) ||// right
+			((row == int(h/4)) && ((col > w/4) && (col < int(w*0.75)))) || // top
+			((row == int(h*0.75)) && ((col > w/4) && (col < int(w*0.75)))) ){ //bottom
+			out[row][col] = 255;
+		    }else{
+			out[row][col] = img[row][col];
+		    }
 		}
 	}
 
@@ -89,7 +89,7 @@ void frame(string input){
 void scale(string input){
     int img[MAX_H][MAX_W];
 	int h, w;
-
+	
 	readImage(input, img, h, w);
 	int out[MAX_H*2][MAX_W*2];
 
@@ -111,11 +111,11 @@ void pixelate(string input){
 
 	for(int row = 0; row < h; row+=2) {
 		for(int col = 0; col < w; col+=2) {
-            int rounded = round((img[row][col] + img[row+1][col] + img[row][col+1] + img[row+1][col+1])/4 +0.5);
-			out[row][col] = rounded;
-            out[row+1][col] = rounded;
-            out[row][col+1] = rounded;
-            out[row+1][col+1] = rounded;
+		    int rounded = round((img[row][col] + img[row+1][col] + img[row][col+1] + img[row+1][col+1])/4 +0.5);
+		    out[row][col] = rounded;
+		    out[row+1][col] = rounded;
+		    out[row][col+1] = rounded;
+		    out[row+1][col+1] = rounded;
 		}
 	}
 
@@ -131,13 +131,13 @@ void kernel(string input){
 
 	for(int row = 0; row < h; row++) {
 		for(int col = 0; col < w; col++) {
-            int f = sobel(img[row-1][col-1],img[row-1][col],img[row-1][col+1],img[row][col-1],img[row][col],img[row][col+1],img[row+1][col-1],img[row+1][col],img[row+1][col+1]);
-            if(f < 0){
-                f = 0;
-            }else if(f > 255){
-                f = 255;
-            }
-            out[row][col] = f;
+		    int f = sobel(img[row-1][col-1],img[row-1][col],img[row-1][col+1],img[row][col-1],img[row][col],img[row][col+1],img[row+1][col-1],img[row+1][col],img[row+1][col+1]);
+		    if(f < 0){
+			f = 0;
+		    }else if(f > 255){
+			f = 255;
+		    }
+		    out[row][col] = f;
 		}
 	}
 
@@ -146,6 +146,7 @@ void kernel(string input){
 
 int main() {
     std::string input = "image1.pgm";
+	
     invert(input);
     invertHalf(input);
     box(input);
